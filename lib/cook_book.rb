a8a2b49383446fafa5025f22f54cb4a3f5d6f9d1
Astrid Hecht <silentaviary@gmail.com>
2022-08-02 12:00:43 -0600

    Times up, almost have iteration 4 solved

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -16,4 +16,18 @@ class CookBook
   def highest_calorie_meal
     @recipes.max_by{ |recipe| recipe.total_calories}
   end
+
+  def date
+    time = Time.new
+    "#{time.month}-#{time.day}-#{time.year}"
+  end
+
+  def summary
+    summary = []
+    recip_name = @recipes.map {|recip| recip.name}
+    ing = @recipes[0].ingredients_required
+    ingredients = Hash[ing.map {|ing| {ingredient: ing[0].name, amount: "#{ing[1]} #{ing[0].unit}", total_calories: "test" }}]
+    require'pry';binding.pry
+  end
+
 end
