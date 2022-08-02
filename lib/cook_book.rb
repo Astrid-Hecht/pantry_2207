f4cf602983f1ac272dc233317a6e3bc4b87023e0
Astrid Hecht <silentaviary@gmail.com>
2022-08-02 12:58:18 -0600

    feat: Iteration 4 complete, over-time

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -24,10 +24,15 @@ class CookBook
 
   def summary
     summary = []
-    recip_name = @recipes.map {|recip| recip.name}
-    ing = @recipes[0].ingredients_required
-    ingredients = Hash[ing.map {|ing| {ingredient: ing[0].name, amount: "#{ing[1]} #{ing[0].unit}", total_calories: "test" }}]
-    require'pry';binding.pry
+    @recipes.each do |recipe|
+      hash = Hash.new
+      hash[:name] = recipe.name
+      hash[:details] = recipe.ingredients_required.reduce(Hash.new) do |hash2, ingredient|
+        hash2[:ingredients] = recipe.ingredients_required.map { |ingred| { ingredient: ingred[0].name, amount: "#{ingred[1]} #{ingred[0].unit}" } }
+        hash2[:total_cal] = recipe.total_calories
+        hash2
+      end
+      summary << hash
+    end
   end
-
 end
