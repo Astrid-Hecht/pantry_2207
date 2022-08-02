e12e987080d190fdbad660af429eb5a845923591
Astrid Hecht <silentaviary@gmail.com>
2022-08-02 09:42:11 -0600

    test: write recipe tests

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,17 +1,35 @@
-require_relative '../lib/stat_tracker'
+require_relative '../lib/ingredient'
+require_relative '../lib/pantry'
+require_relative '../lib/recipe'
 
-# RSpec.describe StatTracker do
-#   before(:all) do
-#     game_path = './data/games.csv'
-#     team_path = './data/teams.csv'
-#     game_teams_path = './data/game_teams.csv'
+RSpec.describe Recipe do
+  before(:all) do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+  end
 
-#     locations = {
-#       games: game_path,
-#       teams: team_path,
-#       game_teams: game_teams_path
-#     }
+  it 'exists' do
+    expect(@recipe1).to be_instance_of(Reciperecipe1)
+  end
 
-#     @stat_tracker = StatTracker.from_csv(locations)
-#   end
+  it 'has a name' do
+    expect(@recipe1.name).to eq("Mac and Cheese")
+  end
+
+  it 'has initally empty required ingredients hash' do
+    expect(@recipe1.ingredients_required).to eq({})
+  end
+
+  it 'can add ingredients' do
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    expect(@recipe1.ingredients_required).to eq({@ingredient1 => 6, @ingredient2 => 8})
+  end
+
+  it 'can list ingrdients without quantity' do 
+    expect(@recipe1.ingredients).to eq([@ingredient1, @ingredient2])
+  end
+end
 
