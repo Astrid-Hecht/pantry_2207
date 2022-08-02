75e776d3b088193db19b51c257a80243a1ac1fa6
Astrid Hecht <silentaviary@gmail.com>
2022-08-02 09:25:39 -0600

    feat: Ingredient complete

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,2 +1,19 @@
 class Ingredient
+  attr_reader :info_hash
+
+  def initialize(info_hash)
+    @info_hash = info_hash
+  end
+
+  def name
+    @info_hash[:name]
+  end
+
+  def unit
+    @info_hash[:unit]
+  end
+
+  def calories
+    @info_hash[:calories]
+  end
 end
